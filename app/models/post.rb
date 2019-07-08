24e53c77d978a159a3aa7e7c04cb771009291ec3
lieby weintraub <liebahinda@gmail.com>
2019-07-08 16:31:04 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,11 @@
 class Post < ActiveRecord::Base
-   validates :title, presence: true
-   validates :content, length: {minimum: 250}
-	validates :summary, length: {maximum: 250}
-	validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
-	 
-	 CLICKBAIT_PATTERNS = [
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
+
+  CLICKBAIT_PATTERNS = [
     /Won't Believe/i,
     /Secret/i,
     /Top [0-9]*/i,
@@ -16,5 +17,4 @@ class Post < ActiveRecord::Base
       errors.add(:title, "must be clickbait")
     end
   end
-
 end
